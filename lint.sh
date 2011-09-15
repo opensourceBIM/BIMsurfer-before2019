b897b3a11ff51c2d7a1bf5586133803009ac7c54
Rehno Lindeque <errantkid@gmail.com>
2011-09-15 22:59:54 +0200

    Added some additional documentation to the script

--- lint.sh
+++ lint.sh
@@ -1,1 +1,5 @@
+# This script is helpful for checking 
+
+# x == [] is always false since javascript is comparing references rather than values
+# Use x.length == 0 instead
 grep "\=\= \[\]" src/* -r
