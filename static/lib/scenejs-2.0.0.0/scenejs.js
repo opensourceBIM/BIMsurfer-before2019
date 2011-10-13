2884d7ab45005fbe422f0f0987c56fde5a0d3a8d
Rehno Lindeque <errantkid@gmail.com>
2011-10-13 09:29:37 +0200

    Upgrade to the latest scenejs (which fixes a bug with remove node)

--- static/lib/scenejs-2.0.0.0/scenejs.js
+++ static/lib/scenejs-2.0.0.0/scenejs.js
@@ -820,7 +820,7 @@ SceneJS._Node.prototype.disconnectNodes = function() {
 SceneJS._Node.prototype.removeNodes = function() {
     var children = this.disconnectNodes();
     for (var i = 0; i < children.length; i++) {
-        this.children[i].destroy();
+        children[i].destroy();
     }
 };
 
