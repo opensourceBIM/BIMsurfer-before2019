8614e36ea4694ef4bbb3b5e83abccba989714970
Ruben de Laat <ruben@logic-labs.nl>
2014-02-12 10:13:18 +0100

    Added Java nature to project and made it a BIMserver plugin (again)

---
+++ src/org/bimsurfer/BimSurferWebModulePlugin.java
@@ -0,0 +1,41 @@
+package org.bimsurfer;
+
+import org.bimserver.plugins.PluginException;
+import org.bimserver.plugins.PluginManager;
+import org.bimserver.plugins.web.AbstractWebModulePlugin;
+
+public class BimSurferWebModulePlugin extends AbstractWebModulePlugin {
+
+	private boolean initialized;
+
+	@Override
+	public void init(PluginManager pluginManager) throws PluginException {
+		super.init(pluginManager);
+		initialized = true;
+	}
+
+	@Override
+	public String getDescription() {
+		return "BIMsurfer";
+	}
+
+	@Override
+	public String getDefaultName() {
+		return "BIMsurfer";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0";
+	}
+
+	@Override
+	public boolean isInitialized() {
+		return initialized;
+	}
+
+	@Override
+	public String getDefaultContextPath() {
+		return "/bimsurfer";
+	}
+}
