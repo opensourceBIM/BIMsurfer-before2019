f4b0fb5c26ce20fadbc4062a8b44d9892a6d90ff
Ruben de Laat <ruben@logic-labs.nl>
2016-10-14 08:40:30 +0200

    Made query readable

--- bimsurfer/src/BimServerModel.js
+++ bimsurfer/src/BimServerModel.js
@@ -44,7 +44,74 @@ define(["bimsurfer/lib/text"], function(text) {
                     resolve(self.tree);
                 }    
 				
-				var query = {"defines":{"Representation":{"type":"IfcProduct","field":"Representation"},"ContainsElementsDefine":{"type":"IfcSpatialStructureElement","field":"ContainsElements","include":{"type":"IfcRelContainedInSpatialStructure","field":"RelatedElements","includes":["IsDecomposedByDefine","ContainsElementsDefine","Representation"]}},"IsDecomposedByDefine":{"type":"IfcObjectDefinition","field":"IsDecomposedBy","include":{"type":"IfcRelDecomposes","field":"RelatedObjects","includes":["IsDecomposedByDefine","ContainsElementsDefine","Representation"]}}},"queries":[{"type":"IfcProject","includes":["IsDecomposedByDefine","ContainsElementsDefine"]},{"type":"IfcRepresentation","includeAllSubtypes":true},{"type":"IfcProductRepresentation"},{"type":"IfcPresentationLayerWithStyle"},{"type":"IfcProduct","includeAllSubtypes":true},{"type":"IfcProductDefinitionShape"},{"type":"IfcPresentationLayerAssignment"},{"type":"IfcRelAssociatesClassification","includes":[{"type":"IfcRelAssociatesClassification","field":"RelatedObjects"},{"type":"IfcRelAssociatesClassification","field":"RelatingClassification"}]},{"type":"IfcSIUnit"},{"type":"IfcPresentationLayerAssignment"}]};
+				var query = 
+					{
+						defines:{
+							Representation:{
+								type: "IfcProduct",
+								field: "Representation"
+							},
+							ContainsElementsDefine: {
+								type: "IfcSpatialStructureElement",
+								field:"ContainsElements",
+								include:{
+									type:"IfcRelContainedInSpatialStructure",
+									field:"RelatedElements",
+									includes:[
+							            "IsDecomposedByDefine",
+							            "ContainsElementsDefine",
+							            "Representation"
+							        ]}
+								},
+							IsDecomposedByDefine: {
+				            	type: "IfcObjectDefinition",
+				            	field:"IsDecomposedBy",
+				            	include: {
+				            		type:"IfcRelDecomposes",
+				            		field:"RelatedObjects",
+				            		includes:[
+		            		            "IsDecomposedByDefine",
+		            		            "ContainsElementsDefine",
+		            		            "Representation"
+		            		        ]
+								}
+							},
+						},
+						queries:[{
+    		            	type:"IfcProject",
+       		            	includes:[
+								"IsDecomposedByDefine",
+								"ContainsElementsDefine"
+							]} , {
+    		            		type:"IfcRepresentation",
+    		            		includeAllSubtypes:true
+    		            	},{
+   		            			type:"IfcProductRepresentation"
+   		            		},{
+		            			type:"IfcPresentationLayerWithStyle"
+		            		},{
+	            				type:"IfcProduct",
+    		            		includeAllSubtypes:true
+    		            	}, {
+    		            		type:"IfcProductDefinitionShape"
+    		            	}, {
+    		            		type:"IfcPresentationLayerAssignment"
+    		            	}, {
+    		            		type:"IfcRelAssociatesClassification",
+    		            		includes:[{
+    		            			type:"IfcRelAssociatesClassification",
+                                    field:"RelatedObjects"
+                                }, {
+ 		            				type:"IfcRelAssociatesClassification",
+ 		            				field:"RelatingClassification"
+    		            		}]
+    		            	}, {
+    		            		type:"IfcSIUnit"
+    		            	}, {
+    		            		type:"IfcPresentationLayerAssignment"
+    		            	}
+    		            ]
+					};
 				
                 // Perform the download
 				model.queryNew(query, function(o) {}).done(function(){		
